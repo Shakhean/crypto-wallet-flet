d3d488f4041f1ed64538871477eab6311d5d9a2b
Shakhean <105813035+Shakhean@users.noreply.github.com>
2026-03-10 14:02:08 +0200

    Update actions.py

--- actions.py
+++ actions.py
@@ -15,11 +15,12 @@ def main(page: ft.Page):
         ft.Column([
             ft.Text("Welcome to My App"),
             ft.Row([
-                ft.ElevatedButton("Buy", on_click=handle_click("Buy", "https://www.moonpay.com/buy")),
+                ft.ElevatedButton("Deposite", on_click=handle_click("Buy")),
                 ft.ElevatedButton("Earn", on_click=handle_click("Earn")),
-                ft.ElevatedButton("Sell", on_click=handle_click("Sell")),
+                ft.ElevatedButton("Withdraw", on_click=handle_click("Sell")),
             ], alignment=ft.MainAxisAlignment.CENTER)
         ], alignment=ft.MainAxisAlignment.CENTER)
     )
 
 ft.app(target=main)
+
